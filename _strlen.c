1472ff15b18f19ac3e944f98727cc452d32491c3
esszakaria3 <esszakaria2@gmail>
2023-09-19 16:46:23 -0700

    prinf

---
+++ _strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - custon function Returns the length of a string.
+ * @s: Type char pointer
+ * Return: c.
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+		;
+	return (c);
+
+}
