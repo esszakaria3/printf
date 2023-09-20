aa8ee0ddb80a1f48783e3b2bd042389a1d05e691
oulaiz-hasna <hasnaoulaiz97@gmail.com>
2023-09-19 17:17:10 -0700

    Adding _printf

---
+++ _printf.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * _printf - custom function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
+ */
+int _printf(const char * const format, ...)
+{
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%i", printf_int}, {"%d", printf_decimal}, {"%b", printf_binary},
+		{"%o", printf_octal}, {"%x", printf_hexa}, {"%X", printf_HEXA},
+
+	};
+
+	va_list args;
+	int i = 0, j, len = 0;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+Here:
+	while (format[i] != '\0')
+	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		len++;
+		i++;
+	}
+	va_end(args);
+	return (len);
+}
+
